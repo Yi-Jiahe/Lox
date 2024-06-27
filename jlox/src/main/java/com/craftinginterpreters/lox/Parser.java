526d9d8423d113349f682f20f4bd88a72c928330
yijiahe <yi.jiahe00@gmail.com>
2024-06-27 07:19:44 +0000

    Implement scope

--- jlox/src/main/java/com/craftinginterpreters/lox/Parser.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -33,10 +33,12 @@ class Parser {
   varDecl        → "var" IDENTIFIER ( "=" expression )? ";" ;
 
   statement      → exprStmt
-                 | printStmt ;
+                 | printStmt
+                 | block ;
 
   exprStmt       → expression ";" ;
   printStmt      → "print" expression ";" ;
+  block          → "{" declaration* "}" ;
 
   expression     → assignment ;
   assignment     → IDENTIFIER "=" assignment
@@ -70,6 +72,7 @@ class Parser {
 
   private Stmt statement() {
     if (match(PRINT)) return printStatement();
+    if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
     return expressionStatement();
   }
@@ -97,6 +100,17 @@ class Parser {
     Expr expr = expression();
     consume(SEMICOLON, "Expect ';' after expression.");
     return new Stmt.Expression(expr);
+  }
+
+  private List<Stmt> block() {
+    List<Stmt> statements = new ArrayList<>();
+
+    while (!check(RIGHT_BRACE) && !isAtEnd()) {
+      statements.add(declaration());
+    }
+
+    consume(RIGHT_BRACE, "Expect '}' after block.");
+    return statements;
   }
 
   private Expr assignment() {
