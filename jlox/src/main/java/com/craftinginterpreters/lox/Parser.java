3e095093b0454b3a60e20edc25f8d6bfb281a8d2
yijiahe <yi.jiahe00@gmail.com>
2024-07-01 05:42:17 +0000

    Implement return statement

--- jlox/src/main/java/com/craftinginterpreters/lox/Parser.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -42,6 +42,7 @@ class Parser {
                  | forStmt
                  | ifStmt
                  | printStmt
+                 | returnStmt
                  | whileStmt
                  | block ;
 
@@ -53,6 +54,7 @@ class Parser {
                  ( "else" statement )? ;
   printStmt      → "print" expression ";" ;
   whileStmt      → "while" "(" expression ")" statement ;
+  returnStmt     → "return" expression? ";" ;
   block          → "{" declaration* "}" ;
 
   expression     → assignment ;
@@ -93,6 +95,7 @@ class Parser {
     if (match(FOR)) return forStatement();
     if (match(IF)) return ifStatement();
     if (match(PRINT)) return printStatement();
+    if (match(RETURN)) return returnStatement();
     if (match(WHILE)) return whileStatement();
     if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
@@ -159,6 +162,17 @@ class Parser {
     Expr value = expression();
     consume(SEMICOLON, "Expect ';' after value.");
     return new Stmt.Print(value);
+  }
+
+  private Stmt returnStatement() {
+    Token keyword = previous();
+    Expr value = null;
+    if (!check(SEMICOLON)) {
+      value = expression();
+    }
+
+    consume(SEMICOLON, "Expect ';' after return value.");
+    return new Stmt.Return(keyword, value);
   }
 
   private Stmt varDeclaration() {
